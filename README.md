c65feb04a0d51f69accabfaf1646492d2660392f
Tullingemarcus <marcus@tullingeskog.se>
2021-01-14 12:28:55 +0100

    changes to readme and finallizing of the code

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ This is a projekt that is supposed to be able to detect sound using a sound sens
 
 * Arduino UNO
 * Soundsensor
-* Relay
 * Ledlamp
 
 ## How it works
-The Soundsensor takes in sound and stores it as a value, i then set a limit to that value. If the value goes above that limit a function will activate and start a process that then turns on a ledlamp.
+The Soundsensor takes in sound and stores it as a value, i then set a limit to that value. If the value goes above that limit a function will activate and start a process that then turns on a ledlamp. If the ledlamp is already on this same process will intead turn of the ledlamp.
 
 ## Software
 
 * Platform.io
     * Makes the program run the code on the arduino
+
+## Schematic
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,26 @@
 #include <Arduino.h>
 
 int Sensor = A0;
-int Relay = 2;
 int Lamp = 13;
-
-
-unsigned long time;
 
 boolean status_lights = false;
 
 void setup() {
   pinMode(Sensor, INPUT);
   pinMode(Lamp, OUTPUT);
-  pinMode(Relay, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   int status_sensor = analogRead(Sensor);
   if (status_sensor > 33) {
-    time = millis();
     if (status_lights == false) {
       status_lights = true;
       digitalWrite(Lamp, HIGH);
-      //delay(5000);
-      //digitalWrite(Relay, HIGH);
     }
-    //else {
-      //status_lights = false;
-      //digitalWrite(Lamp, LOW);
-      //delay(100);
-    //}
+    else {
+      status_lights = false;
+      digitalWrite(Lamp, LOW);
+    }
   }
-  Serial.println(time);
 }
